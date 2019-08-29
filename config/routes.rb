5a42f75126ff8a90f6c876ebac5102068d10f9bf
jeanarm <nizigiarmel2015@gmail.com>
2019-08-29 14:05:34 +0200

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :profiles
   devise_for :users, controllers: {registrations: "registrations"}
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
